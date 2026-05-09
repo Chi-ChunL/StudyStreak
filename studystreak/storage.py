7778af12fe7974cb27f2b78cefabd93580957dec
Chi-ChunL <austin8182009@gmail.com>
2026-05-09 22:43:17 +0100

    Improved storage

--- studystreak/storage.py
+++ studystreak/storage.py
@@ -4,15 +4,28 @@ from pathlib import Path
 
 DATA_FILE = Path("study_data.json")
 
+
+def get_deafault_data():
+    return {
+        "sessions": [],
+        "weekly_goal": 300
+    }
+
 #load the study data from the json file
 def load_data():
     if not DATA_FILE.exists():
-        return {
-            "sessions": []
-        }
+        return get_deafault_data()
     
     with open(DATA_FILE, "r") as file:
         return json.load(file)
+    
+    if "sessions" not in data:
+        data["sessions"] = []
+    
+    if "weekly_goal" not in data:
+        data["weekly_goal"] = []
+    
+    return data
 
 #saves study data into json file 
 def save_data(data):
